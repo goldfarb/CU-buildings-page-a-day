b14b4a529bac5f33ca5e4dc27172100f8d4bbe53
Alice Goldfarb <a_f_goldfarb@yahoo.com>
2026-02-15 23:36:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
-function setup() {
-  createCanvas(400, 400);
+let img_r;
+
+function preload(){
+  img_r = loadImage('img/right.png');
 }
 
-function draw() {
-  background(20);
+function setup() {
+  createCanvas(400, 400);
+  
+  background(200,100,60);
+  
+  img_r.loadPixels();
+
+      for (let i = 0; i < img_r.pixels.length; i += 4) {
+        let r = img_r.pixels[i];
+        let g = img_r.pixels[i + 1];
+        let b = img_r.pixels[i + 2];
+
+        img_r.pixels[i] = 0;   // New R
+        img_r.pixels[i + 1] = 110; // New G
+        img_r.pixels[i + 2] = 255; // New B
+    }
+
+  
+  img_r.updatePixels();
+  
+  
+  image(img_r, 0, 0, width, height, 0, 0, img_r.width, img_r.height, CONTAIN);
+  
 }
